232a0a9056d1b64ef6a0cd8e55b2bf3f5f4d6a74
johntayona <johntayona14@gmail.com>
2025-08-26 18:48:41 -0700

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoOGS0VGTMDbjjn26pXnvxUubucAcr830",
+  authDomain: "jlt-goals.firebaseapp.com",
+  projectId: "jlt-goals",
+  storageBucket: "jlt-goals.firebasestorage.app",
+  messagingSenderId: "549012573966",
+  appId: "1:549012573966:web:a79d4856b7b1caace0115f"
+};
+
+const app = initializeApp(firebaseConfig);
