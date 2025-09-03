47f201b02d7e4a3c4da3f9c17ea646d907e8964e
johntayona <johntayona14@gmail.com>
2025-09-02 18:28:02 -0700

    Create goals

--- app/goals/_layout.jsx
+++ app/goals/_layout.jsx
@@ -6,39 +6,39 @@ export default function GoalsLayout() {
 
   return (
     <GoalsProvider>
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: 'black',
-        tabBarInactiveTintColor: 'grey',
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Your Goals',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons 
-              size={24} 
-              name={focused ? 'home' : 'home-outline'} 
-              color="black"
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: 'Create Goal',
-          tabBarIcon: ({ focused }) => (
-            <Ionicons 
-              size={24} 
-              name={focused ? 'create' : 'create-outline'} 
-              color="black"
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'grey',
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Your Goals',
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                size={24}
+                name={focused ? 'home' : 'home-outline'}
+                color="black"
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: 'Create Goal',
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                size={24}
+                name={focused ? 'create' : 'create-outline'}
+                color="black"
+              />
+            ),
+          }}
+        />
+      </Tabs>
     </GoalsProvider>
   )
 }
